89ca10377dab1935a584acfdddd8a96a569b47e9
floretn <iesofronov@sberinsur.ru>
2023-02-22 16:43:34 +0300

    start vue js

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
     <a href="https://www.youtube.com/@VladilenMinin">Владиленом Мининым
 </a></h3>
 
-<b><a href="https://youtu.be/aQkgUUmUJy4">1 часть занятий</a></b></br>
-<b><a href="https://youtu.be/Ti2Q4sQkNdU">2 часть занятий</a></b>
+<b><a href="https://youtu.be/aQkgUUmUJy4">1 часть занятий (База JS)</a></b></br>
+<b><a href="https://youtu.be/Ti2Q4sQkNdU">2 часть занятий (Повторение JS и просто знаний)</a></b></br>
+<b><a href="https://youtu.be/NQ2i5cZ87ug">3 часть занятий (Vue.js)</a></b>
---
+++ part-3/vue-test/src/router/index.js
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },{
+    path: '/login',
+    name: 'login',
+    meta: {layout: 'empty'},
+    component: () => import('@/views/LoginView.vue')
+  },{
+    path: '/categories',
+    name: 'categories',
+    meta: {layout: 'main'},
+    component: () => import('@/views/CategoriesView.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
