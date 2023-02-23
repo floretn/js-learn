22e1e92c2f0a8a6a48bdd17409474a593ab01953
floretn <iesofronov@sberinsur.ru>
2023-02-23 14:44:40 +0300

    переход по страницам работает

---
+++ part-3/vue-test/src/components/app/SidebarView.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul class="sidenav app-sidenav" :class="{open: isOpen}">
+    <router-link
+      v-for="link in links"
+      :key="link.url"
+      :to="link.url"
+      active-class="active"
+      exact
+    >
+      <li>
+        <a href="#" class="waves-effect waves-orange pointer">{{link.title}}</a>
+      </li>
+    </router-link>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['isOpen'],
+  data: () => ({
+    links: [
+      {title: 'Счет', url: '/', exact: false},
+      {title: 'История', url: '/history'},
+      {title: 'Планирование', url: '/planning'},
+      {title: 'Новая запись', url: '/record'},
+      {title: 'Категории', url: '/categories'}
+    ]
+  })
+}
+</script>
--- part-3/vue-test/src/router/index.js
+++ part-3/vue-test/src/router/index.js
@@ -1,21 +1,56 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    meta: {layout: 'main'},
+    component: () => import('@/views/HomeView.vue')
   },{
     path: '/login',
     name: 'login',
     meta: {layout: 'empty'},
     component: () => import('@/views/LoginView.vue')
   },{
+    path: '/register',
+    name: 'register',
+    meta: {layout: 'empty'},
+    component: () => import('@/views/RegisterView.vue')
+  },{
     path: '/categories',
     name: 'categories',
     meta: {layout: 'main'},
     component: () => import('@/views/CategoriesView.vue')
+  },{
+    path: '/detail-record',
+    name: 'detail-record',
+    meta: {layout: 'main'},
+    component: () => import('@/views/DetailRecordView.vue')
+  },{
+    path: '/history',
+    name: 'history',
+    meta: {layout: 'main'},
+    component: () => import('@/views/HistoryView.vue')
+  },{
+    path: '/planning',
+    name: 'planning',
+    meta: {layout: 'main'},
+    component: () => import('@/views/PlanningView.vue')
+  },{
+    path: '/profile',
+    name: 'profile',
+    meta: {layout: 'main'},
+    component: () => import('@/views/ProfileView.vue')
+  },{
+    path: '/record',
+    name: 'record',
+    meta: {layout: 'main'},
+    component: () => import('@/views/RecordView.vue')
+  },{
+    path: "/:pathMatch(.*)*",
+    name: 'error',
+    meta: {layout: 'main'},
+    component: () => import("@/views/ErrorView.vue")
   }
 ]
 
